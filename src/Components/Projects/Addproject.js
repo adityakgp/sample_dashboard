00b5dc176dff391bc648baa429f79df147293b7b
adityakgp <adityadas.c24@gmail.com>
2022-05-05 13:56:18 +0530

    linked sheets to projects & add projects

--- src/Components/Projects/Addproject.js
+++ src/Components/Projects/Addproject.js
@@ -1,49 +1,76 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 import Sidebar from '../Sidebar/Sidebar'
 import Topbar from '../Topbar/Topbar'
 
-function Addproject() {
+export default class Addproject extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       title: '',
+       link: '',
+       description: ''
+    }
+  }
+
+  changeHandler = (e) => {
+    this.setState({[e.target.name] : e.target.value})
+  }
+
+  submitHandler = e => {
+    e.preventDefault();
+    console.log(this.state);
+
+    axios.post('https://sheet.best/api/sheets/3bde586f-9a90-496e-857e-d7d180f6988e', this.state)
+    .then(response => {
+      console.log(response);
+    })
+  }
+  render(){
+    const {title, link, description} = this.state;
   return (
         <main className='main-dashboard'>
         <div className='main-topbar'><Topbar/></div>
         <div className='main-sidebar'><Sidebar /></div>
         <div className='content'>
 
-        <section class="text-green-500 body-font relative">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-col text-center w-full mb-12">
-      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-green-500">Add New Project</h1>
+        <section className="text-green-500 body-font relative">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-col text-center w-full mb-12">
+      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-green-500">Add New Project</h1>
     </div>
-    <div class="lg:w-1/2 md:w-2/3 mx-auto">
-      <div class="flex flex-wrap -m-2">
-        <div class="p-2 w-1/2">
-          <div class="relative">
-            <label for="name" class="leading-7 text-sm text-green-500">Project Title</label>
-            <input type="text" id="name" name="name" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+    <form onSubmit={this.submitHandler} >
+    <div className="lg:w-1/2 md:w-2/3 mx-auto">
+      <div className="flex flex-wrap -m-2">
+        <div className="p-2 w-1/2">
+          <div className="relative">
+            <label for="title" className="leading-7 text-sm text-green-500">Project Title</label>
+            <input type="text" id="title" name="title" value={title} onChange={this.changeHandler} className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
           </div>
         </div>
-        <div class="p-2 w-1/2">
-          <div class="relative">
-            <label for="link" class="leading-7 text-sm text-green-500">Project Link</label>
-            <input type="text" id="email" name="link" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+        <div className="p-2 w-1/2">
+          <div className="relative">
+            <label for="link" className="leading-7 text-sm text-green-500">Project Link</label>
+            <input type="url" id="link" name="link" value={link} onChange={this.changeHandler} className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
           </div>
         </div>
-        <div class="p-2 w-full">
-          <div class="relative">
-            <label for="description" class="leading-7 text-sm text-green-500">Project Description</label>
-            <textarea id="description" name="description" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+        <div className="p-2 w-full">
+          <div className="relative">
+            <label for="description" className="leading-7 text-sm text-green-500">Project Description</label>
+            <textarea id="description" name="description" value={description} onChange={this.changeHandler} className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
           </div>
         </div>
-        <div class="p-2 w-full">
-          <button class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">ADD</button>
+        <div className="p-2 w-full">
+          <button type='submit' className="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">ADD</button>
         </div>
       </div>
     </div>
+    </form>
   </div>
 </section>
         </div>
     </main>
   )
+  }
 }
-
-export default Addproject
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ProjectCard from './ProjectCard'
 
-function Projects(props) {
+function Projects() {
+  const [project, setproject] = useState([]);
+  async function Updateproject() {
+  const res = await fetch(`https://sheet.best/api/sheets/3bde586f-9a90-496e-857e-d7d180f6988e`);
+  res.json().then(res => setproject(res))
+  }
+  useEffect(() => {
+    Updateproject();
+  },[]);
   return (
     <main>
-        
-        {/* <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900 pt-5 px-5">Available Projects</h1> */}
-        
-        <ProjectCard/>
+      <section className="text-gray-600 body-font">
+      <div className="container px-5 py-5 mx-auto">
+      <div className="flex flex-nowrap -m-4">
+      {project.map((data)=>{
+        return(
+          <ProjectCard
+            title={data.title}
+            link={data.link}
+            description={data.description}
+          />
+        )
+      })}
+      </div>
+      </div>
+      </section>
     </main>
   )
 }
